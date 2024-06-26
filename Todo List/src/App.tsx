92e36fda3c7a8eec29b8cdfd406510a42c988cbb
JuniorrBraga <160857840+JuniorrBraga@users.noreply.github.com>
2024-06-25 23:33:40 -0300

    Add files via upload

---
+++ Todo List/src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ToDo from './Components/ToDo'
+import Todo from './Components/Todo.css'
+import Input from './Components/input'
+
+
+
+function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function addTodo() {
+
+    if (todo.trim() !== "") {
+      setTodos([...todos, todo]); 
+      setTodo("");
+    }
+  }
+
+  return (
+    <>
+      <header>
+        <h1 className='title'>Todo List</h1>
+      </header>
+
+      <p>
+        <input type="text" placeholder="Digite Aqui" value={todo} onChange={(e) => setTodo(e.target.value)} />
+        <button onClick={addTodo}>Salvar</button>
+      </p>
+
+      <ul>
+        {todos.map((todo, index) => {
+          return <ToDo key={index} todo={todo} />;
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App;
